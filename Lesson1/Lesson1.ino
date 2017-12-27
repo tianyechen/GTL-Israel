452b81f525b826240f22f6d184cf550e52320382
tianyechen <16tc31@gmail.com>
2017-12-26 23:36:09 -0500

    -m

--- Lesson1/Lesson1.ino
+++ Lesson1/Lesson1.ino
@@ -1,9 +1,36 @@
+#include <Wire.h>
+
+// assign variables to pin numbers
+const int buttonPin = 1;
+const int LEDPin = 9;
+
+// state variable
+int buttonState = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  // tell arduino what kind of pins they are
+  pinMode(buttonPin, INPUT);
+  pinMode(LEDPin, OUTPUT);
 
+  // set up for debugging 
+  Wire.begin();
+  // set baud rate for serial communication
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  // check value of buttonPin
+  buttonState = digitalRead(buttonPin);
 
+  // logic to turn on and off the LED
+  if (buttonState == HIGH){
+    digitalWrite(LEDPin, HIGH);
+  }
+  else{
+    digitalWrite(LEDPin, LOW);
+  }
+ 
 }
